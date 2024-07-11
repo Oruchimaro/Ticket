e38e5e334a681998722108e0da66396ef9abbbda
Amir Daneshkar <amir.daneshkar@hotmail.com>
2024-07-11 23:44:11 +0330

    Added Ticket Resource

---
+++ app/Filament/Admin/Resources/TicketResource.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Filament\Admin\Resources;
+
+use App\Enums\TicketPriorityEnum;
+use App\Enums\TicketStatusEnum;
+use App\Filament\Admin\Resources\TicketResource\Pages;
+use App\Models\Ticket;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class TicketResource extends Resource
+{
+    protected static ?string $model = Ticket::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->columns(1)
+            ->schema([
+                TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Textarea::make('description')
+                    ->rows(3)
+                    ->columnSpanFull(),
+                Section::make('Select')
+                    ->columns(2)
+                    ->schema([
+                        Radio::make('status')
+                            ->required()
+                            ->options(TicketStatusEnum::class),
+                        Radio::make('priority')
+                            ->required()
+                            ->options(TicketPriorityEnum::class),
+                        Select::make('assigned_to')
+                            ->relationship('assignedTo', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->optionsLimit(10)
+                            ->required(),
+                        Select::make('assigned_by')
+                            ->relationship('assignedBy', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->optionsLimit(10)
+                            ->required(),
+                    ]),
+                Textarea::make('comment')
+                    ->rows(3)
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('assigned_to')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('assigned_by')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('priority')
+                    ->badge()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTickets::route('/'),
+            'create' => Pages\CreateTicket::route('/create'),
+            'edit' => Pages\EditTicket::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Admin/Resources/TicketResource/Pages/CreateTicket.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\TicketResource\Pages;
+
+use App\Filament\Admin\Resources\TicketResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTicket extends CreateRecord
+{
+    protected static string $resource = TicketResource::class;
+}
