631adb588e727edda609a25dd2ce678b55845c4d
Amir Daneshkar <amir.daneshkar@hotmail.com>
2024-08-06 19:35:05 +0330

    Make sidebar collapse

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,6 +54,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+        // ->sidebarFullyCollapsibleOnDesktop()
+            ->sidebarCollapsibleOnDesktop();
     }
 }
