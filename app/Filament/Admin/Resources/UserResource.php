e5d5320ba96ba85d5bd25e3bbd90a986c89c1936
Amir Daneshkar <amir.daneshkar@hotmail.com>
2024-08-06 20:31:49 +0330

    Add custom action for password update

--- app/Filament/Admin/Resources/UserResource.php
+++ app/Filament/Admin/Resources/UserResource.php
@@ -32,11 +32,7 @@ class UserResource extends Resource
                     ->required()
                     ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
-                Forms\Components\TextInput::make('password')
-                    ->password()
-                    ->required()
-                    ->maxLength(255),
-            ]);
+            ])->columns(1);
     }
 
     public static function table(Table $table): Table
--- app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Admin/Resources/UserResource/Pages/EditUser.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
+use Filament\Actions\Action;
+use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
 
 class EditUser extends EditRecord
@@ -12,6 +15,21 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('updatePassword')
+                ->form([
+                    TextInput::make('password')->required()->password()->confirmed(),
+                    TextInput::make('password_confirmation')->required()->password(),
+                ])
+                ->action(function (array $data) {
+                    $this->record->update([
+                        'password' => $data['password'], // is hashed in casts
+                    ]);
+
+                    Notification::make()
+                        ->title(__('Message Updated Successfully.'))
+                        ->success()
+                        ->send();
+                }),
         ];
     }
 }
