124cb766ad1f9a41bdacd795165a593d2d4cce9a
Amir Daneshkar <amir.daneshkar@hotmail.com>
2024-08-09 20:25:21 +0330

    Added colors

--- app/Filament/Admin/Resources/TextMessageResource.php
+++ app/Filament/Admin/Resources/TextMessageResource.php
@@ -16,7 +16,7 @@ class TextMessageResource extends Resource
 {
     protected static ?string $model = TextMessage::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-left-ellipsis';
 
     public static function form(Form $form): Form
     {
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,9 +27,15 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->registration()
+            ->font('Oswald')
             ->colors([
-                'primary' => Color::Amber,
                 'secondary' => Color::Blue,
+                'danger' => Color::Rose,
+                'gray' => Color::Gray,
+                'info' => Color::Blue,
+                'primary' => Color::Indigo,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
             ])
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\\Pages')
