321ed77f99f2fe8da84982bae825f072f9c79ff6
Amir Daneshkar <amir.daneshkar@hotmail.com>
2024-08-30 00:16:01 +0330

    fix issues with husky

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,19 +3,30 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
-     *
      * @test
      */
-    public function the_application_returns_a_successful_response(): void
+    public function the_application_root_path_redirects_unauthenticated_user_to_login(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
+    public function the_application_root_path_redirects_authenticated_user_to_tickets_route(): void
+    {
+        $user = User::factory()->make();
+        $response = $this->actingAs($user)->get('/');
+
+        $response->assertRedirect(route('tickets.index'));
     }
 }
